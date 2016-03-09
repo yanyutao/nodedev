b45771c964ce282d0b4d4866a54a34cd21297d96
yanyutao <867528315@qq.com>
2016-03-09 21:20:03 +0800

    删除.idea

--- server.js
+++ server.js
@@ -6,48 +6,30 @@ var urlnode = require("url");
 var server = http.createServer(serverStart);
 
 var persondata = {
-    "person": [
-        {
-            "name": "闫宇韬",
-            "age": "26",
-            "des": "H5前端开发"
-        },
-        {
-            "name": "闫宇韬",
-            "age": "26",
-            "des": "H5前端开发"
-        },
-        {
-            "name": "闫宇韬",
-            "age": "26",
-            "des": "H5前端开发"
-        }
-    ]
+    "person": []
 };
 
 function serverStart(request,response){
     console.log(request.method);
     console.log(request.url);
-
+	
+	//读取html css js文件
     readHtml(request,response);
 
-
     //添加json数据
-
     var urlObj = urlnode.parse(request.url,true);
     if(urlObj.query.method == 'addperson'){
-
-
-            persondata.person.push(urlObj.query);
-
-            response.writeHead(200, {'Content-Type': 'application/json'});
-            response.write('{"msg":"添加成功"}');
-            console.log(persondata)
-            response.end();
-
+        persondata.person.push(urlObj.query);
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.write('{"msg":"添加成功"}');
+        response.end();
+        console.log(persondata)
+    }else if(urlObj.query.method == 'searchperson'){
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.write(JSON.stringify(persondata));
+        response.end();
     }
     console.log(urlObj)
-
 
 };
 
@@ -65,19 +47,25 @@ function readHtml(request,response){
     response.setHeader('Content-Type',mime.lookup(url)+';chartset=utf-8');
 
     //读取打开所有的页面html css js
-    fs.readFile('.'+url,'utf8',function(err,data){
-        if(err){
-            response.statusCode = 404;
-            response.end();
-            console.log(err);
+    fs.exists('.'+url,function(exists){
+        if(exists){
+            fs.readFile('.'+url,'utf8',function(err,data){
+                if(err){
+                    response.statusCode = 404;
+                    response.end();
+                    console.log(err);
+                }else{
+                    response.statusCode = 200;
+                    response.write(data);
+                    response.end();
+                }
+            })
         }else{
-            response.statusCode = 200;
-            response.write(data);
+            response.statusCode = 404;
             response.end();
         }
     })
-}
 
-
+};
 
 server.listen(8080,"localhost");
