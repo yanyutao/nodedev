4468e20f90683d36fe46e7f12326cb007351e1f7
yanyutao <867528315@qq.com>
2016-03-09 23:48:27 +0800

    添加copy.js和concat.js

---
+++ module/copy.js
@@ -0,0 +1,16 @@
+/**
+ * 1. 把src里的文件内容，写入到target文件里
+ * 2.写入时要先清空再写入
+ */
+
+'use strict'
+var fs = require('fs');
+function copy(src,target){
+    fs.readFile(src,function (err,data) {
+        fs.writeFile(target,data,function(err){
+            console.log("copy success from " + src + "to " + target);
+        }) ;
+    });
+}
+copy("./atm.jpg",'./atm2.jpg');
+copy('./name.txt','./name.bak.txt');
